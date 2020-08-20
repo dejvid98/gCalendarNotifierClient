a7c2077dbb37ea1e86449f6885b26039831a0e92
dejvid98 <52608605+dejvid98@users.noreply.github.com>
2020-08-20 12:28:05 +0200

    Initial commit

---
+++ build/precache-manifest.61218197610e950a73925a232ee1a004.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707d2ba3a76a9f605dd26bc689f7c192",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53daae483637841da7c2",
+    "url": "/static/css/main.dfaf883e.chunk.css"
+  },
+  {
+    "revision": "fe8517775d0071179374",
+    "url": "/static/js/2.d7463be2.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/static/js/2.d7463be2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53daae483637841da7c2",
+    "url": "/static/js/main.b63b8484.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
